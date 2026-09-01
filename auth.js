7b406c3b4ff1f805a2fc39fb1205a6ba495a270b
student2eng <student2eng@gmail.com>
2026-09-01 18:23:06 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,8 +3,8 @@
 // =====================================================
 
 // Supabase configuration
-const SUPABASE_URL = "0000000";
-const SUPABASE_KEY = "000000000";
+const SUPABASE_URL = "https://azuzgodrxkxhlsekooyc.supabase.co";
+const SUPABASE_KEY = "sb_publishable_urenPm0k3KqkSpb9aSkVOw_OVYch9mM";
 
 // Create Supabase client
 const supabaseClient = window.supabase.createClient(
